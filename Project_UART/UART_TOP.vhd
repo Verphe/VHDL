92560e57c9fe232be204d1d06e3d9955e27ec608
Fosen1 <ffosen@hotmail.com>
2025-11-28 14:53:25 +0100

    Tester ut signaler for å dra en kabel fra en entity til en annen

--- Project_UART/UART_TOP.vhd
+++ Project_UART/UART_TOP.vhd
@@ -8,8 +8,6 @@ entity uart_top is
         clk          : in  std_logic;                     --Systemklokke (50MHz)
         reset        : in  std_logic;                     --Reset
         rx           : in  std_logic;                     --UART RX pin
-        data_out_rx     : out std_logic_vector(7 downto 0);  --Mottatt databyte
-        data_rdy_rx     : out std_logic;                      --Puls når databyte er klar
         rx_led      : out std_logic;                      --LED pin for mottatt data
         tx           : out std_logic;                     --UART TX pin 
         HEX0        : out std_logic_vector(7 downto 0);    --Hex displayer
@@ -21,8 +19,9 @@ end uart_top;
 architecture arch of uart_top is
     signal sample_tick  : std_logic;                     --Sample tick (8x baud)
     --signal q_count      : std_logic_vector(9 downto 0);  --Teller for baud rate generator (Kan brukes for output)
-    
-
+    signal done_tick_to_setflag : std_logic;
+    signal rx_data_to_save_byte : std_logic_vector(7 downto 0);
+    signal save_byte_to_display : std_logic_vector(7 downto 0);
 begin
     --Baud generator
     --50 MHz, 9600 Baud, 8x oversampling
@@ -53,8 +52,8 @@ begin
             reset          => reset,
             rx           => rx,
             sample_tick  => sample_tick,
-            rx_done_tick => data_rdy_rx,
-            data_out     => data_out_rx
+            rx_done_tick => done_tick_to_setflag,
+            data_out     => rx_data_to_save_byte
             --o_rx_led     => rx_led 
         );
 
@@ -64,9 +63,9 @@ begin
             clk       => clk,
             reset     => reset,
             clr_flag  => '0', --Ikke bruk klar flagg
-            set_flag  => data_rdy_rx, --Sett flagg når data er mottatt
-            data_in   => data_out_rx, --Data inngang fra UART RX
-            data_out  => data_out_rx, --Data utgang
+            set_flag  => done_tick_to_setflag, --Sett flagg når data er mottatt
+            data_in   => rx_data_to_save_byte, --Data inngang fra UART RX
+            data_out  => save_byte_to_display, --Data utgang
             flag_out  => open --Ikke bruk flagg utgang
         );
     --Kontroller for display
@@ -74,7 +73,7 @@ begin
         port map (
             clk        => clk,
             reset      => reset,
-            data_in    => data_out_rx,
+            data_in    => save_byte_to_display,
             -- data_ready => data_rdy,
             HEX0       => HEX0,
             HEX1       => HEX1,
