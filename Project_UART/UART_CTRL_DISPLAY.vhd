d29689f5457b06288addf8e72bb009063c20be06
Fosen1 <ffosen@hotmail.com>
2025-11-28 13:40:57 +0100

    La til reset og klokke

--- Project_UART/UART_CTRL_DISPLAY.vhd
+++ Project_UART/UART_CTRL_DISPLAY.vhd
@@ -5,8 +5,8 @@ use ieee.numeric_std.all;
 
 entity UART_CTRL_DISPLAY is
     port (
-        --clk            : in  std_logic;                      -- 50 MHz
-        --reset          : in  std_logic;
+        clk            : in  std_logic;                      -- 50 MHz
+        reset          : in  std_logic;
         --data_ready     : in  std_logic;
         data_in        : in  std_logic_vector(7 downto 0);   -- Mottatt byte
         HEX0           : out std_logic_vector(7 downto 0);   
@@ -19,10 +19,11 @@ architecture rtl of UART_CTRL_DISPLAY is
 
 
     signal ascii_val : integer range 0 to 255;
-	 signal hundreds   : integer range 0 to 9;
+	signal hundreds   : integer range 0 to 9;
     signal tens      : integer range 0 to 9;
     signal ones      : integer range 0 to 9;
 
+    
     -- 7-segment aktive-lav
     type seg_array is array (0 to 9) of std_logic_vector(7 downto 0);
     constant SEG : seg_array := (
@@ -39,17 +40,25 @@ architecture rtl of UART_CTRL_DISPLAY is
     );
 
 begin
+    process(clk, reset)
+    begin
+        if reset = '1' then
+            HEX0 <= (others => '1');
+            HEX1 <= (others => '1');
+            HEX2 <= (others => '1');
+        elsif rising_edge(clk) then
+        -- Konverter 8-bit -> ASCII heltall
+        ascii_val <= to_integer(unsigned(data_in)); 
 
-    -- Konverter 8-bit -> ASCII heltall
-    ascii_val <= to_integer(unsigned(data_in)); 
+        -- Del i desimaler
+        hundreds <= (ascii_val /100) mod 10; --Tredje siffer
+        tens <= (ascii_val / 10) mod 10; -- Andre siffer
+        ones <= ascii_val mod 10;        -- Første siffer
 
-    -- Del i desimaler
-	hundreds <= (ascii_val /100) mod 10; --Tredje siffer
-    tens <= (ascii_val / 10) mod 10; -- Andre siffer
-    ones <= ascii_val mod 10;        -- Første siffer
-
-    -- Display utganger
-	HEX2 <= SEG(hundreds); 
-	HEX1 <= SEG(tens);
-	HEX0 <= SEG(ones);
+        -- Display utganger
+        HEX2 <= SEG(hundreds); 
+        HEX1 <= SEG(tens);
+        HEX0 <= SEG(ones);
+        end if;
+    end process;
 end rtl;
