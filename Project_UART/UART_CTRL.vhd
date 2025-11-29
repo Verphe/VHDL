c6fdb6fde88aee07ddcec1338c133f7b58be2bd6
Fosen1 <ffosen@hotmail.com>
2025-11-29 15:58:26 +0100

    Skal kunne sende en melding nå?

--- Project_UART/UART_CTRL.vhd
+++ Project_UART/UART_CTRL.vhd
@@ -26,12 +26,27 @@ entity UART_CTRL is
 end UART_CTRL;
 
 architecture rtl of UART_CTRL is
-    constant FIXED_BYTE : std_logic_vector(7 downto 0) := b"01000011";
+    --constant FIXED_BYTE : std_logic_vector(7 downto 0) := b"01000011"; --Sender en C ved knappetrykk
+
+    type t_string is array(0 to 7) of std_logic_vector(7 downto 0);
+    constant message : t_string := (
+        b"01001000", -- H
+        b"00111101", -- a
+        b"00111101", -- a
+        b"01101011", -- k
+        b"01101111", -- o
+        b"01101110", -- n
+        b"00111010", -- :
+        b"00011101"  -- )
+    );
+
+    signal index_reg, index_next : integer range 0 to 7 := 0;
 
     signal tx_buf_reg     : std_logic_vector(7 downto 0);
     signal tx_buf_next    : std_logic_vector(7 downto 0);
     signal tx_flag_reg, tx_flag_next : std_logic; --Utgangsflagg
     
+
 
     begin
 
@@ -40,6 +55,7 @@ architecture rtl of UART_CTRL is
         if reset = '1' then
             tx_buf_reg   <= (others => '0');
             tx_flag_reg  <= '0';
+            index_reg    <= 0;
         elsif rising_edge(clk) then
             tx_buf_reg   <= tx_buf_next;
             tx_flag_reg  <= tx_flag_next;
@@ -64,10 +80,16 @@ architecture rtl of UART_CTRL is
     
     else
         if button_press = '1' then
-            tx_buf_next <= FIXED_BYTE;
-            tx_flag_next <= '1';
-        else
-            tx_flag_next <= '0';
+            if rx_flag = '1' then --Vent på at forrige byte er ferdig
+                tx_buf_next <= message(index_reg);
+                tx_flag_next <= '1';
+
+                if index_reg = 7 then
+                    index_next <= 0; --Reset index etter siste byte
+                else
+                    index_next <= index_reg + 1; --Neste byte i meldingen
+                end if;
+            end if;
         end if;
     end if;
     end process;
